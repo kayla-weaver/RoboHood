5f1a16248aed2f7c35e60df5b389fb7ff52f6b5c
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-09 00:00:20 -0700

    fix all biz logic.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,16 @@
 // business logic 
-
-
-window.onload = function () {
   
   function checkInp(num) {
     let result = [];  
     if (!isNaN(num)) {
       for (let index= 0; index <= num; index += 1){
         let newNum = index.toString().split("");
-        if (newNum.includes("1")){
-          result.push("Beep!");
+        if (newNum.includes("3")){
+          result.push("Won't you be my neighbor");
+        } else if (newNum.includes("2")){
+          result.push("Boop!");
+        } else if (newNum.includes("1")){
+          result.push("Beep!!")
         } else {
           result.push(index);
         }
@@ -20,10 +21,17 @@ window.onload = function () {
     return result;
   }
 
-};
-
 
 
 
 //user interface logic 
 
+function handleFormSubmission(e) {
+  e.preventDefault();
+  const input = document.getElementById("PutYaNumba").value; 
+  document.getElementById("new-nums").textContent = input;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#input").addEventListener("submit", handleFormSubmission);
+});
